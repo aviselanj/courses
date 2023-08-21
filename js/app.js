33f5fc2c992298db8626944997da0cd6f64d2db4
Avielle Lewis <aviselanj@gmail.com>
2023-08-21 14:15:12 -0500

    edit indentation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 //Greeting 
 let userName = prompt("Hello, Im Avi! What's your name?");
 alert("Im glad you are here " + userName + "! Let's play a little guessing game all about me.");
@@ -11,56 +11,64 @@ let pastJobsPrompt = prompt("Is Avi a Uninted States Veteran?");
 let animalPrompt = prompt("Does Avi have a dog?");
 let jobPropmt = prompt("Does Avi have a job?");
 
+userName;
+interestPrompt;
+whereFromPrompt;
+pastJobsPrompt;
+animalPrompt;
+jobPropmt;
 
 // Display the user's answers and update score
 let playerPoints = 0;
 
 function playGame() {
-    function gameChoice() {
+  function gameChoice() {
 
-        if (interestPrompt == "yes") {
-            alert("By George, I think she's got it! " + interestPrompt + ", Avi loves to code!");
-            playerPoints++
-        } else {
-            alert("You are so wrong about that!");
-        }
-        if (whereFromPrompt == "yes") {
-            alert("You are so damn smart. Avi's family history can be traced back 200 years ago in New Orleans from Haiti.");
-            playerPoints++
-        } else {
-            alert("Wrong! Avi was indeed born and raised in New Orleans and her family has been here for over 2 centuries.");
-
-        }
-        if (pastJobsPrompt == "yes") {
-            alert("Wow! How did you guess? Yes, Avi was honorably discharged as a Human Resources Specialist of the United States Army.");
-            playerPoints++
-        } else {
-            alert("Wrong, Avi is a proud veteran of the United States Army.");
-        }
-        if (animalPrompt == "no") {
-            alert("Thats correct. Avi is a cat lady");
-            playerPoints++
-        } else {
-            alert("Wrong, Avi is a cat lady.");
-        }
-        if (jobPropmt == "no") {
-            alert("Your right! Avi refuses to work unless she's coding.");
-            playerPoints++
-        } else {
-            alert("Your wrong, Unfortunately Avi has not as of yet gotten paid to code.");
-        }
-
-    }
-    gameChoice()
-    prompt("One last question, how many years have I been studying code?");
-    if (prompt < 4) {
-        alert("That is too low.")
-    } else if (propt > 4) {
-        alert("That is too high.")
+    if (interestPrompt === "yes") {
+      alert("By George, I think she's got it! " + interestPrompt + ", Avi loves to code!");
+      playerPoints++;
     } else {
-        alert("Spot on!")
+      alert("You are so wrong about that!");
+    }
+    if (whereFromPrompt === "yes") {
+      alert("You are so damn smart. Avi's family history can be traced back 200 years ago in New Orleans from Haiti.");
+      playerPoints++;
+    } else {
+      alert("Wrong! Avi was indeed born and raised in New Orleans and her family has been here for over 2 centuries.");
+
+    }
+    if (pastJobsPrompt === "yes") {
+      alert("Wow! How did you guess? Yes, Avi was honorably discharged as a Human Resources Specialist of the United States Army.");
+      playerPoints++;
+    } else {
+      alert("Wrong, Avi is a proud veteran of the United States Army.");
+    }
+    if (animalPrompt == "no") {
+      alert("Thats correct. Avi is a cat lady");
+      playerPoints++;
+    } else {
+      alert("Wrong, Avi is a cat lady.");
+    }
+    if (jobPropmt === "no") {
+      alert("Your right! Avi refuses to work unless she's coding.");
+      playerPoints++;
+    } else {
+      alert("Your wrong, Unfortunately Avi has not as of yet gotten paid to code.");
     }
 
-    console.log(playerPoints);
+  }
+  gameChoice()
+  prompt("One last question, how many years have I been studying code?");
+  if (prompt < 4) {
+    alert("That is too low.")
+  } else if (propt > 4) {
+    alert("That is too high.")
+  } else {
+    alert("Spot on!");
+  }
 
-    playerPointsPara.innerHTML = playerPoints;
+  console.log(playerPoints);
+
+  playerPointsPara.innerHTML = playerPoints;
+}
+playGame();
